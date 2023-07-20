cc12bbbfc114731d3b81e80192114b17c0a31479
elliotclowes <elliot@clowes.me>
2023-07-20 11:28:42 +0100

    If appearance is null show it as 'unknown'

--- src/components/SuperheroCard.jsx
+++ src/components/SuperheroCard.jsx
@@ -6,18 +6,23 @@ const SuperheroCard = ({ superhero }) => {
     return null;
   }
 
-  const { powerstats } = superhero;
+  const { powerstats, appearance } = superhero;
 
   return (
     <div>
       <h2>{superhero.name}</h2>
-      <img src={superhero.image.url} alt={superhero.name} />
+      <img src={superhero.image.url} alt={superhero.name} width="200px" />
+      <h3>Stats</h3>
       {Object.entries(powerstats)
         .filter(([stat, value]) => value !== null)
         .map(([stat, value]) => (
           <p key={stat}>{stat}: {value}</p>
-        ))
-      }
+        ))}
+      <h3>Appearance</h3>
+      {appearance.gender ? <p>Gender: {appearance.gender}</p> : <p>Gender: Unknown</p>}
+      {appearance.race ? <p>Race: {appearance.race}</p> : <p>Race: Unknown</p>}
+      {appearance.height[0] !== '-' ? <p>Height: {appearance.height[0]}</p> : <p>Height: Unknown</p>}
+      {appearance.weight[0] !== '- lb' ? <p>Weight: {appearance.weight[0]}</p> : <p>Weight: Unknown</p>}
     </div>
   );
 };
