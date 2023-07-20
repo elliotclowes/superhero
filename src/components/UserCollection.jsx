eded4587c47f84997c0b350122bdd5cb9518d64f
elliotclowes <elliot@clowes.me>
2023-07-20 14:18:39 +0100

    Don't show user collection if they have no cards

--- src/components/UserCollection.jsx
+++ src/components/UserCollection.jsx
@@ -1,6 +1,10 @@
 const UserCollection = ({ collectedSuperheroes }) => {
+
+    let hasCollection = collectedSuperheroes.length === 0 ? '' : 'Your Collection'
+
     return (
         <div>
+            <h2>{hasCollection}</h2>
             {collectedSuperheroes.map(superhero => (
                 <p key={superhero.id}>{superhero.name}</p>
             ))}
